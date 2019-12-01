2c1266a9c5bd53f4439d8dc4bc984f30a0e0b92b
andreibuntsev <andrew.buntsev@gmail.com>
2019-12-02 06:49:24 +1100

    Updated the blog link

--- src/components/common/pages.tsx
+++ src/components/common/pages.tsx
@@ -8,7 +8,7 @@ export const PAGES: Array<PAGE> = [
   { title: 'About', route: '/' },
   { title: 'Experiences', route: '/experiences' },
   { title: 'Projects', route: '/projects' },
-  { title: 'Blog', externalLink: 'http://andrewbuntsev.blogspot.com/' },
+  { title: 'Blog', externalLink: 'http://andreibuntsev.blogspot.com/' },
   { title: 'Profile', route: '/profile' },
   { title: 'Contacts', route: '/contacts' }
 ];

961d6eb7a0592ca26f273e5fea5414ed59220111
andreibuntsev <andrew.buntsev@gmail.com>
2019-12-30 07:14:45 +1100

    Rewrote about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,17 +4,21 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { HashLink } from 'react-router-hash-link';
 
-const linkStyle: CSSProperties = { color: '#145DA0', fontWeight: 'bold', textDecoration: 'none' };
+const linkStyle: CSSProperties = {
+  color: '#145DA0',
+  fontWeight: 'bold',
+  textDecoration: 'none'
+};
 
-const P = styled.p`
-  text-align: justify;
-`;
+const P = styled.p`text-align: justify;`;
 
 class About extends React.Component {
   render() {
     return (
       <div>
-        <h2 className="title" id='top'>About Me</h2>
+        <h2 className="title" id="top">
+          About Me
+        </h2>
         <hr className="hr" />
 
         <img
@@ -25,41 +29,38 @@ class About extends React.Component {
         />
         <P>
           I started my IT career working as a system administrator at Russian
-          Railways Branch in 2005. Back then, I fell in love with computers,
-          networks, information systems and anything related to IT. After about
-          a year I discovered that my real passion was programming. I started
-          coding using different languages - VB, Perl, JavaScripr, CiCode, C#,
-          1C, Lisp, Flash. Being a junior, I tried every programming language I
+          Railways in 2005. I instantly fell in love with computers, networks,
+          information systems and anything related to IT. After about a year I
+          discovered that my real passion was programming. I started coding
+          using different languages - VB, Perl, JavaScript, CiCode, C#, 1C,
+          Lisp, Flash. Being a junior, I tried every programming language I
           heard about. Of course, not everything has gone to production..
         </P>
 
         <P>
-          After a while, I was hired by the Australian employer (Asia-Australia
-          Technology), and was working as a developer for 10 years. During that
-          time I evolved from junior to Senior Full Stack Developer,
-          participated in about 8 various projects as Front End / Back End /
-          Full Stack developer (<HashLink
+          For the last 10 years I have been employed by an Australian employer
+          (Asia-Australia Technology). During my tenure I evolved from a junior
+          to a Senior Full Stack Developer, participated in about 8 various
+          projects as a Front End / Back End / Full Stack developer (<HashLink
             to="/experiences#careersHeader"
             style={linkStyle}
           >
             Experiences
-          </HashLink>). Most of work has been done by me remotely because I lived
-          in Russia until 2017.
+          </HashLink>). Most of work was done remotely from Russia until I made
+          the move to Australia in December 2017 to work full time for Nine
+          Entertainment Co.
         </P>
 
         <P>
-          By the time I moved to Australia (Dec 2017) my preferable stack was
-          desktop-oriented client-server apps (.NET, C#, WPF, WCF, MSSQL).
-          Starting working for Channel Nine I found myself really passionate
-          about programming in JavaScript. So currently being a Full Stack
-          JavaScript Developer, I develop web-based{' '}
-          <NavLink
-            to="/projects/orion"
-            style={linkStyle}
-          >
+          At this time my preferable stack was desktop-oriented client-server
+          apps (.NET, C#, WPF, WCF, MSSQL). Once commencing full time work for
+          Channel Nine I found myself really passionate about programming in
+          JavaScript and now spend the majority of my time as a Full Stack
+          JavaScript Developer developing web-based{' '}
+          <NavLink to="/projects/orion" style={linkStyle}>
             apps
           </NavLink>{' '}
-          using NodeJS, ExpressJS, React, Redux, TypeScript technologies.
+          using NodeJS, ExpressJS, React, Redux & TypeScript technologies.
         </P>
 
         <hr className="hr" />

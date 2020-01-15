0078dacf0f1b79b4b7efdd9507934b4d5e9b5b03
andreibuntsev <andrew.buntsev@gmail.com>
2020-01-16 09:35:27 +1100

    Added Resume

--- src/components/common/pages.tsx
+++ src/components/common/pages.tsx
@@ -8,6 +8,7 @@ export const PAGES: Array<PAGE> = [
   { title: 'About', route: '/' },
   { title: 'Experiences', route: '/experiences' },
   { title: 'Projects', route: '/projects' },
+  { title: 'Resume', externalLink: '/Resume.pdf' },
   { title: 'Blog', externalLink: 'https://andreibuntsev.blogspot.com/' },
   { title: 'Recommendations', route: '/recommendations' } //,
   //{ title: 'Contacts', route: '/contacts' }

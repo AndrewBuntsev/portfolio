437bba5c97972bbf38a0cb24ee033a7dd2b040cc
andreibuntsev <andrew.buntsev@gmail.com>
2020-02-16 21:23:23 +1100

    Made references clickable

--- src/components/RecommendationCard.tsx
+++ src/components/RecommendationCard.tsx
@@ -9,32 +9,38 @@ type Props = {
 };
 type State = {};
 class RecommendationCard extends React.Component<Props, State> {
-
   render() {
     return (
       <div className="recommendationCard">
         <a href={this.props.link} target="_blank" rel="noopener noreferrer">
           <img
             style={{ borderRadius: '50%', marginTop: '15px' }}
-            alt='person'
+            alt="person"
             src={require('./../assets/recommendations/' + this.props.picture)}
             width="60"
           />
         </a>
-        <div
-          style={{
-            width: '180px',
-            margin: '20px 15px 0 15px'
-          }}
+        <a
+          href={this.props.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: 'none', color: '#000000' }}
         >
-          <span style={{ fontWeight: 'bold' }}>
-            {this.props.name}
-          </span>
-          <br />
-          <span style={{ fontSize: '12px' }}>
-            {this.props.position}
-          </span>
-        </div>
+          <div
+            style={{
+              width: '180px',
+              margin: '20px 15px 0 15px'
+            }}
+          >
+            <span style={{ fontWeight: 'bold' }}>
+              {this.props.name}
+            </span>
+            <br />
+            <span style={{ fontSize: '12px' }}>
+              {this.props.position}
+            </span>
+          </div>
+        </a>
         <div style={{}}>
           <blockquote />
           <div
